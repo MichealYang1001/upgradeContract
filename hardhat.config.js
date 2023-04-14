a4b482eadc342362267a6743d32e613e01fdfa9f
micheal yang <shulin.yang@matrixport.com>
2023-04-14 23:33:55 +0800

    hardhatconfig

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,44 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
 
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-etherscan");
+const PRIVATE_KEY = ""; // 您的私钥
+const INFURA_PROJECT_ID = "YOUR_INFURA_PROJECT_ID"; // 您的 Infura 项目 ID
+const BSC_TESTNET_RPC_URL = `https://data-seed-prebsc-1-s1.binance.org:8545`; // BSC Testnet RPC 地址
 
-require("./tasks/task")
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    testnet: {
+      url: BSC_TESTNET_RPC_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
   solidity: {
-    version: "0.8.17",
-
-
-    viaIR: true,
+    version: "0.8.7",
     settings: {
       optimizer: {
-
         enabled: true,
-        runs: 200
-      }, outputSelection: {
-        "*": {
-          "*": ["evm.assembly", "irOptimized"],
-        }
-      }
-    }
-  },
-
-  defaultNetwork: "BSCTest",
-  networks: {
-    hardhat: {
-
-    },
-    BSCTest: {
-      url: "https://bsc-testnet.public.blastapi.io",
-      accounts: [],
-
+        runs: 200,
+      },
     },
   },
-  etherscan: {
-    apiKey: "", // Your Etherscan API key
-  }
 };
-
-// "*":{
-//   "*":["evm.assembly","irOptimized"],
-// }
